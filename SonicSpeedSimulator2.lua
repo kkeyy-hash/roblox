e3112ed8782239a4936de8951790b9b8c1174798
kkeyy <83645517+kkeyy-hash@users.noreply.github.com>
2022-08-04 00:58:27 +0200

    Update SonicSpeedSimulator2.lua

--- SonicSpeedSimulator2.lua
+++ SonicSpeedSimulator2.lua
@@ -1,6 +1,3 @@
-local allah = 0
-
 for i = 1,9 do
-    allah = allah + 1
-    game:GetService('ReplicatedStorage').Knit.Services.RewardService.RF.GiveRewardInBank:InvokeServer(tostring(allah))
+    game:GetService('ReplicatedStorage').Knit.Services.RewardService.RF.GiveRewardInBank:InvokeServer(tostring(i))
 end
