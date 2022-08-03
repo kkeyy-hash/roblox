c1075bcff63d283ff1f152eca0e0319e65a3130b
kkeyy <83645517+kkeyy-hash@users.noreply.github.com>
2022-08-03 23:59:15 +0200

    Update Maid.lua

--- Maid.lua
+++ Maid.lua
@@ -1,42 +1,50 @@
 local Maid = {}
 Maid.ClassName = "Maid"
 
+
 function Maid.new()
-    local self = {}
-    self._tasks = {}
+	local self = {}
 
-    return setmetatable(self, Maid)
+	self._tasks = {}
+
+	return setmetatable(self, Maid)
 end
 
-function Maid.Destroy()
-    self:CleanUp()
-    setmetatable(self, nil)
-end
+function Maid:GiveTask(task)
+	assert(task)
+	local taskId = #self._tasks+1
+	self[taskId] = task
 
-function Maid:GiveTask(Task)
-    print("Received task")
-    assert(Task, "[CLEARDD] Task is required to proceed")
-    local TaskID = #self._tasks + 1
-    self[TaskID] = Task
-
-    if type(task) == "table" and (not task.Destroy) then
-        return error("[CLEARDD] Maid.GiveTask Error, function or RBXScriptConnection expected, got table")
-    end
-
-    return TaskID
-end
-
-function Maid:CleanUp()
-    for I, V in ipairs(self._tasks) do
-        self._tasks[I] = nil
-	if typeof(V) == "RBXScriptConnection" then
-	    V:Disconnect()
-        elseif type(V) == "function" then
-            V()
-        elseif V.Destroy then
-            V:Destroy()
+	if type(task) == "table" and (not task.Destroy) then
+		warn("[Maid.GiveTask] - Gave table task without .Destroy\n\n" .. debug.traceback())
 	end
-    end
+
+	return taskId
 end
 
+function Maid:DoCleaning()
+	local tasks = self._tasks
+
+	for index, task in pairs(tasks) do
+		if typeof(task) == "RBXScriptConnection" then
+			tasks[index] = nil
+			task:Disconnect()
+		end
+	end
+
+	local index, task = next(tasks)
+	while task ~= nil do
+		tasks[index] = nil
+		if type(task) == "function" then
+			task()
+		elseif typeof(task) == "RBXScriptConnection" then
+			task:Disconnect()
+		elseif task.Destroy then
+			task:Destroy()
+		end
+		index, task = next(tasks)
+	end
+end
+
+Maid.Destroy = Maid.DoCleaning
 return Maid
